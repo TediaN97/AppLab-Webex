523688dda5eadf181e12458014a91770facc03fd
Matus Sabat <matus.sabat@student.tuke.sk>
2024-04-24 12:07:10 +0200

    first-page

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AppLab</title>
+    <link rel="stylesheet" href="css/main.min.css">
+</head>
+<body class="text-primary">
+    <header class="container mt-5">
+        <nav class="navbar navbar-expand-lg ">
+            <a class="navbar-brand fs-1" href="#intro">
+                <span class="fw-bold text-secondary">App</span><span class="fw-bold text-primary">Lab</span>
+            </a>
+            <button class="navbar-toggler border-primary" type="button" data-bs-toggle="collapse" data-bs-target="#main-nav" aria-controls="main-nav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-md-end" id="main-nav">
+                <ul class="navbar-nav justify-content-evenly w-75">
+                    <li class="nav-item">
+                        <a class="nav-link text-primary mt-3 fs-6" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-primary mt-3 fs-6" href="#">Key Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-primary mt-3 fs-6" href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-primary mt-3 fs-6" href="#">Testiminial</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-primary mt-3 fs-6" href="#">FAQ</a>
+                    </li>
+                    <button type="button" class="btn btn-tertiary btn-lg rounded-pill px-5 py-3">
+                        <span class="fw-bolder fs-6">Try for free</span></a>
+                    </button>
+                </ul>
+            </div>
+        </nav>
+        <div class="opacity-25 z-n1 position-absolute mt-5 start-0 ms-5">
+            <img src="./public/vectors.webp" alt="vectors" />
+        </div>
+        <div class="d-flex flex-column justify-content-evenly align-items-center flex-xl-row">
+            <div>
+                <h5 class="w-75 rounded-pill bg-gray"><img class="bg-orange rounded-pill p-2" src="./public/correct.webp" alt="correct" /> #1 Editiors Choice App of 2020</h6>
+                <h1 class="fw-bold display-3">Best app for your <br/> modern lifestyle</h1>
+                <h5 class="opacity-75 mt-3">Increase productivity with a simple to-do app. App for <br/> managing your personal budgets.</h5>
+                <div class="mt-4">
+                    <button type="button" class="btn btn-tertiary btn-lg rounded-pill px-5 py-3">
+                        <span class="fw-bolder fs-6">Try for free</span></a>
+                    </button>
+                    <button type="button" class="btn btn-lg text-tertiary rounded-pill">
+                        <span class="fs-6">Watch demo video</span>
+                    </button>
+                </div>
+            </div>
+            <img class="display-2" src="./public/smartphone.webp" alt="smartphone" />
+        </div>
+        <img class="position-absolute z-n1 w-100 fixed-bottom " src="./public/town.webp" alt="town" />
+        <img class="position-absolute z-n1 fixed-bottom ms-5" src="./public/red_circles.webp" alt="red_circles" />
+    </header>
+
+    <script src="node_modules/bootstrap/dist/js/bootstrap.js"></script>
+</body>
+</html>
